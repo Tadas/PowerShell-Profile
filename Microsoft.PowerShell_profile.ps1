80a25623512e9ed8917d169d31348d59a20d79a1
♠ <thetadas+github@gmail.com>
2016-11-05 02:22:33 +0000

    Tweak prompt style

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,13 +4,14 @@ Write-Host "`n`t`"Never send a human to do a machine's job.`"`n"
 
 $global:LastCommandsHistoryId = 0
 
+$ColorSeparator = [char]0xe0b0
 # The custom prompt
 function Prompt {
 	$NextHistId = (Get-History -count 1).Id + 1
 
 	# If command history id wasn't incremented means the last command was empty. The user probably needs some whitespace
 	if ($global:LastCommandsHistoryId -eq $NextHistId){
-		Write-Host "`r`n`r`n`r`n`r`n`r`n"
+		Write-Host "`r`n`r`n`r`n`r`n"
 	} else {
 		# show the last command in the title bar
 		if ((Get-History).count -ge 1) {
@@ -18,14 +19,15 @@ function Prompt {
 			
 		}
 	}
-
+    Write-Host "" # Separator after the output
 	Write-Host "[$(Get-Date -Format T)] $(Get-Location)"  -Background Black -Foreground Green -NoNewLine
 	
 	# Fill the remainder of the line with some color!
 	$RemainingSpace = (Get-Host).UI.RawUI.BufferSize.Width - (Get-Host).UI.RawUI.CursorPosition.X 
 	Write-Host (" " * $RemainingSpace) -Background Black -Foreground Green -NoNewLine
 
-	Write-Host " λ" -NoNewLine -Foreground Gray
+	Write-Host " λ" -NoNewLine -Foreground Black -BackgroundColor DarkGray
+	Write-Host $ColorSeparator -NoNewline -ForegroundColor DarkGray
 	
 	$global:LastCommandsHistoryId = $NextHistId
 	return " "
